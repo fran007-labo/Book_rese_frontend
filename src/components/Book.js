d1c78f7dfdff4863bdd34e5cbccd17141eb7be13
Yutaka Nagasaki <fran.seagal1@gmail.com>
2022-08-16 17:10:37 +0900

    flashMessageを表示

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,21 +1,26 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import instance from "../lib/ApiClient"
+import instance from "../lib/ApiClient";
+import { FlashMessages } from './Index'
 
 export default function Book(props) {
-
+  const [flashMessage, setFlashMessage]  = useState({message: '', status: '', open: false});
+  
   const { id, title, author, body, created_at, imageUrl } = props;
   const lend = (id) => {
-    instance.post(`/books/${id}/add_books`)
+    instance.post(`/books/${id}/add_books`).then(response => {
+      setFlashMessage({message: response.data['message'], status: response.data['status'], open: true})
+    })
   };
-
+  
   return (
     <div>
+      {flashMessage.open && <FlashMessages {...flashMessage}/>}
       <Card
         sx={{ display: 'flex', flexDirection: 'column' }}
         >
---
+++ src/components/Common/FlashMessage.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function FlashMessages(props) {
+  const [open, setOpen] = useState(false);
+  const message = props.message;
+  const status  = props.status;
+
+  useEffect(() => {
+    setOpen(true)
+  },[])
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+  return (
+    <Stack spacing={2} sx={{ width: '100%' }}>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </Stack>
+  );
+}
