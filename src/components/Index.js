6acf71fd34119fa6595649827a5b80c92b90ee84
Yutaka Nagasaki <fran.seagal1@gmail.com>
2022-08-20 19:14:35 +0900

    reactとtypescriptのバージョンを合わせた

--- src/components/Index.js
+++ src/components/Index.js
@@ -4,4 +4,4 @@ export {default as BooksIndex} from './BooksIndex';
 export {default as MenuButton} from './MenuButton';
 export {default as RegiBookForm} from './RegiBookForm';
 export {default as Cart} from './Cart';
-export {default as FlashMessages} from './Common/FlashMessage';
+export {default as FlashMessages} from './Common/FlashMessage.js';
